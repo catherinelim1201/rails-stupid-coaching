4b470f9921623d43246c9c2c77f839d6a26af2aa
Catherine Lim <jkvvhappy@gmail.com>
2023-08-29 13:34:16 +0800

    stupid coaching, bootstrap

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    # @question = params[:ask]
+
+    if params[:ask].include?("?")
+      @answer = 'Silly question, get dressed and go to work!.'
+    elsif params[:ask] == 'I am going to work'
+      @answer = 'Great'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+# params[:ask]
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get "/", to "pages#home"
+  get '/ask', to: 'questions#ask'
+
+  get '/answer', to: 'questions#answer'
 end
